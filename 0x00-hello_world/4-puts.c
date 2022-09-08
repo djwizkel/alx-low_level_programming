6fe3247637e2d9c25ec85eacdbf37fa9b819edc4
djwizkel <deejaywizkel@gmail.com>
2022-09-08 11:26:07 -0700

    Compiling C Program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-   @@ -0,0c+1,12 @@
-1  #include <stdio.h>
-2  /**
-3  * main - main block
-4  * Return: 0
-5  */
-6  int main(void)
-7  {
-8  	puts("\"Programming is like building a multilingual puzzle");
-9  	return (0);
-10  }
+#include <stdio.h>
+/**
+* main - main block
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	

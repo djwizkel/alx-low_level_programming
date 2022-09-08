4baaacd27c7b9d9073e51c90fa12c3e232c2d824
djwizkel <deejaywizkel@gmail.com>
2022-09-08 10:59:48 -0700

    Compiling C Program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+1  #include <stdio.h>
+2  /**
+3  * main - main block
+4  * Return: 0
+5  */
+6  int main(void)
+7  {
+8  	puts("\"Programming is like building a multilingual puzzle");
+9  	return (0);
+10  }

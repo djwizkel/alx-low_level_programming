b17fc91e6410281ef1b09888f99e985511f3fd29
djwizkel <deejaywizkel@gmail.com>
2022-09-21 19:56:47 -0700

    Pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,29 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string in rot13
- * @s: string to be encoded
- * Return: the resulting string
+ * rot13 - Encodes a string using rot13.
+ * @a: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
  */
-
-char *rot13(char *s)
+char *rot13(char *a)
 {
-  int i, j;
-  char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int j;
+	int i = 0;
+	char init[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char fin[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-  for (i = 0; s[i] != '\0'; i++)
-  {
-    for (j = 0; a[j] != '\0'; j++)
-    {
-      if (s[i] == a[j])
-      {
-        s[i] = b[j];
-        break;
-      }
-    }
-  }
-  return (s);
+	while (*(a + i))
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (*(a + i) == init[j])
+			{
+				*(a + i) = fin[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (a);
 }
